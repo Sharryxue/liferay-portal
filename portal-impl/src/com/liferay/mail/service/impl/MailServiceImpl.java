d75f38eab0d7fab0c1c799b9b3ce2b079a67f458
Sharry Shi <sharry.shi@liferay.com>
2022-04-18 22:02:06 +0800

    Revert "LPS-149222 (portal-impl): Allows mail service to be configured by company"
    This reverts commit f1bcb90d8d152148bf5c935411d332c423d7a327.

--- portal-impl/src/com/liferay/mail/service/impl/MailServiceImpl.java
+++ portal-impl/src/com/liferay/mail/service/impl/MailServiceImpl.java
@@ -24,10 +24,7 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.MessageBusUtil;
-import com.liferay.portal.kernel.model.CompanyConstants;
 import com.liferay.portal.kernel.module.framework.service.IdentifiableOSGiService;
-import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.InfrastructureUtil;
 import com.liferay.portal.kernel.util.MethodHandler;
 import com.liferay.portal.kernel.util.MethodKey;
@@ -42,13 +39,10 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
 
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
-
-import javax.portlet.PortletPreferences;
 
 /**
  * @author Brian Wing Shun Chan
@@ -106,17 +100,13 @@ public class MailServiceImpl implements IdentifiableOSGiService, MailService {
 	@Clusterable
 	@Override
 	public void clearSession() {
-		clearSession(CompanyConstants.SYSTEM);
+		_session = null;
 	}
 
 	@Clusterable
 	@Override
 	public void clearSession(long companyId) {
-		if (companyId == CompanyConstants.SYSTEM) {
-			_sessions.clear();
-		}
-
-		_sessions.remove(companyId);
+		_session = null;
 	}
 
 	@Override
@@ -150,98 +140,56 @@ public class MailServiceImpl implements IdentifiableOSGiService, MailService {
 
 	@Override
 	public Session getSession() {
-		long companyId = CompanyThreadLocal.getCompanyId();
-
-		Session session = _sessions.get(companyId);
-
-		if (session != null) {
-			return session;
+		if (_session != null) {
+			return _session;
 		}
 
-		session = InfrastructureUtil.getMailSession();
+		Session session = InfrastructureUtil.getMailSession();
 
-		PortletPreferences companyPreferences = PrefsPropsUtil.getPreferences(
-			companyId);
+		if (!PrefsPropsUtil.getBoolean(
+				PropsKeys.MAIL_SESSION_MAIL, PropsValues.MAIL_SESSION_MAIL)) {
 
-		PortletPreferences systemPreferences = PrefsPropsUtil.getPreferences();
+			_session = session;
 
-		boolean sessionMail = GetterUtil.getBoolean(
-			companyPreferences.getValue(
-				PropsKeys.MAIL_SESSION_MAIL,
-				systemPreferences.getValue(
-					PropsKeys.MAIL_SESSION_MAIL,
-					String.valueOf(PropsValues.MAIL_SESSION_MAIL))));
-
-		if (!sessionMail) {
-			_sessions.put(companyId, session);
-
-			return session;
+			return _session;
 		}
 
-		String advancedPropertiesString = companyPreferences.getValue(
+		String advancedPropertiesString = PrefsPropsUtil.getString(
 			PropsKeys.MAIL_SESSION_MAIL_ADVANCED_PROPERTIES,
-			systemPreferences.getValue(
-				PropsKeys.MAIL_SESSION_MAIL_ADVANCED_PROPERTIES,
-				PropsValues.MAIL_SESSION_MAIL_ADVANCED_PROPERTIES));
-		String pop3Host = companyPreferences.getValue(
+			PropsValues.MAIL_SESSION_MAIL_ADVANCED_PROPERTIES);
+		String pop3Host = PrefsPropsUtil.getString(
 			PropsKeys.MAIL_SESSION_MAIL_POP3_HOST,
-			systemPreferences.getValue(
-				PropsKeys.MAIL_SESSION_MAIL_POP3_HOST,
-				PropsValues.MAIL_SESSION_MAIL_POP3_HOST));
-		String pop3Password = companyPreferences.getValue(
+			PropsValues.MAIL_SESSION_MAIL_POP3_HOST);
+		String pop3Password = PrefsPropsUtil.getString(
 			PropsKeys.MAIL_SESSION_MAIL_POP3_PASSWORD,
-			systemPreferences.getValue(
-				PropsKeys.MAIL_SESSION_MAIL_POP3_PASSWORD,
-				PropsValues.MAIL_SESSION_MAIL_POP3_PASSWORD));
-		int pop3Port = GetterUtil.getInteger(
-			companyPreferences.getValue(
-				PropsKeys.MAIL_SESSION_MAIL_POP3_PORT,
-				systemPreferences.getValue(
-					PropsKeys.MAIL_SESSION_MAIL_POP3_PORT,
-					String.valueOf(PropsValues.MAIL_SESSION_MAIL_POP3_PORT))));
-		String pop3User = companyPreferences.getValue(
+			PropsValues.MAIL_SESSION_MAIL_POP3_PASSWORD);
+		int pop3Port = PrefsPropsUtil.getInteger(
+			PropsKeys.MAIL_SESSION_MAIL_POP3_PORT,
+			PropsValues.MAIL_SESSION_MAIL_POP3_PORT);
+		String pop3User = PrefsPropsUtil.getString(
 			PropsKeys.MAIL_SESSION_MAIL_POP3_USER,
-			systemPreferences.getValue(
-				PropsKeys.MAIL_SESSION_MAIL_POP3_USER,
-				PropsValues.MAIL_SESSION_MAIL_POP3_USER));
-		String smtpHost = companyPreferences.getValue(
+			PropsValues.MAIL_SESSION_MAIL_POP3_USER);
+		String smtpHost = PrefsPropsUtil.getString(
 			PropsKeys.MAIL_SESSION_MAIL_SMTP_HOST,
-			systemPreferences.getValue(
-				PropsKeys.MAIL_SESSION_MAIL_SMTP_HOST,
-				PropsValues.MAIL_SESSION_MAIL_SMTP_HOST));
-		String smtpPassword = companyPreferences.getValue(
+			PropsValues.MAIL_SESSION_MAIL_SMTP_HOST);
+		String smtpPassword = PrefsPropsUtil.getString(
 			PropsKeys.MAIL_SESSION_MAIL_SMTP_PASSWORD,
-			systemPreferences.getValue(
-				PropsKeys.MAIL_SESSION_MAIL_SMTP_PASSWORD,
-				PropsValues.MAIL_SESSION_MAIL_SMTP_PASSWORD));
-		int smtpPort = GetterUtil.getInteger(
-			companyPreferences.getValue(
-				PropsKeys.MAIL_SESSION_MAIL_SMTP_PORT,
-				systemPreferences.getValue(
-					PropsKeys.MAIL_SESSION_MAIL_SMTP_PORT,
-					String.valueOf(PropsValues.MAIL_SESSION_MAIL_SMTP_PORT))));
-		boolean smtpStartTLSEnable = GetterUtil.getBoolean(
-			companyPreferences.getValue(
-				PropsKeys.MAIL_SESSION_MAIL_SMTP_STARTTLS_ENABLE,
-				systemPreferences.getValue(
-					PropsKeys.MAIL_SESSION_MAIL_SMTP_STARTTLS_ENABLE,
-					String.valueOf(
-						PropsValues.MAIL_SESSION_MAIL_SMTP_STARTTLS_ENABLE))));
-		String smtpUser = companyPreferences.getValue(
+			PropsValues.MAIL_SESSION_MAIL_SMTP_PASSWORD);
+		int smtpPort = PrefsPropsUtil.getInteger(
+			PropsKeys.MAIL_SESSION_MAIL_SMTP_PORT,
+			PropsValues.MAIL_SESSION_MAIL_SMTP_PORT);
+		boolean smtpStartTLSEnable = PrefsPropsUtil.getBoolean(
+			PropsKeys.MAIL_SESSION_MAIL_SMTP_STARTTLS_ENABLE,
+			PropsValues.MAIL_SESSION_MAIL_SMTP_STARTTLS_ENABLE);
+		String smtpUser = PrefsPropsUtil.getString(
 			PropsKeys.MAIL_SESSION_MAIL_SMTP_USER,
-			systemPreferences.getValue(
-				PropsKeys.MAIL_SESSION_MAIL_SMTP_USER,
-				PropsValues.MAIL_SESSION_MAIL_SMTP_USER));
-		String storeProtocol = companyPreferences.getValue(
+			PropsValues.MAIL_SESSION_MAIL_SMTP_USER);
+		String storeProtocol = PrefsPropsUtil.getString(
 			PropsKeys.MAIL_SESSION_MAIL_STORE_PROTOCOL,
-			systemPreferences.getValue(
-				PropsKeys.MAIL_SESSION_MAIL_STORE_PROTOCOL,
-				PropsValues.MAIL_SESSION_MAIL_STORE_PROTOCOL));
-		String transportProtocol = companyPreferences.getValue(
+			PropsValues.MAIL_SESSION_MAIL_STORE_PROTOCOL);
+		String transportProtocol = PrefsPropsUtil.getString(
 			PropsKeys.MAIL_SESSION_MAIL_TRANSPORT_PROTOCOL,
-			systemPreferences.getValue(
-				PropsKeys.MAIL_SESSION_MAIL_TRANSPORT_PROTOCOL,
-				PropsValues.MAIL_SESSION_MAIL_TRANSPORT_PROTOCOL));
+			PropsValues.MAIL_SESSION_MAIL_TRANSPORT_PROTOCOL);
 
 		Properties properties = session.getProperties();
 
@@ -313,7 +261,7 @@ public class MailServiceImpl implements IdentifiableOSGiService, MailService {
 		}
 
 		if (smtpAuth) {
-			session = Session.getInstance(
+			_session = Session.getInstance(
 				properties,
 				new Authenticator() {
 
@@ -327,12 +275,10 @@ public class MailServiceImpl implements IdentifiableOSGiService, MailService {
 				});
 		}
 		else {
-			session = Session.getInstance(properties);
+			_session = Session.getInstance(properties);
 		}
 
-		_sessions.put(companyId, session);
-
-		return session;
+		return _session;
 	}
 
 	@Override
@@ -407,6 +353,6 @@ public class MailServiceImpl implements IdentifiableOSGiService, MailService {
 	private static final MethodKey _updatePasswordMethodKey = new MethodKey(
 		Hook.class, "updatePassword", long.class, long.class, String.class);
 
-	private final Map<Long, Session> _sessions = new ConcurrentHashMap<>();
+	private Session _session;
 
 }
