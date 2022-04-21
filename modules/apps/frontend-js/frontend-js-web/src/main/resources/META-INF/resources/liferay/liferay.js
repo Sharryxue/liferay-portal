5a7ea1253cd8611d22e5037c1018d31208e25b0f
Sharry Shi <sharry.shi@liferay.com>
2022-04-21 09:32:38 +0800

    Revert "LPS-150923 empty response object is not the indicator of a bad response"
    This reverts commit ea77ca1bace8ce492b220071e89713cbb4a03864.

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/liferay.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/liferay.js
@@ -123,6 +123,7 @@ Liferay = window.Liferay || {};
 
 			ioConfig.complete = function (response) {
 				if (
+					Object.keys(response).length > 0 &&
 					!Object.prototype.hasOwnProperty.call(response, 'exception')
 				) {
 					if (callbackSuccess) {
